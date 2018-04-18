b8991f6eeac6599403a2b35844dbfc2cd0ca8d29
Owen <owen.dodd@gmail.com>
2018-04-18 15:17:48 -0400

    initial setup

---
+++ learn/tooltips/v1/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Hello} from './AppStyles';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Hello>Hello World</Hello>
+      </div>
+    );
+  }
+}
+
+export default App;
