9937adef46b471307adcd9128ae2b59ee554cbee
Owen <owen.dodd@gmail.com>
2018-04-18 18:01:27 -0400

    simple-react-tooltip

--- learn/tooltips/v1/src/AppStyles.js
+++ learn/tooltips/v1/src/AppStyles.js
@@ -1,6 +1,26 @@
 import styled from 'styled-components';
+import { injectGlobal } from 'styled-components';
+import 'react-tippy/dist/tippy.css';
 
-export const Hello = styled.h1`
-  text-align: center;
-  font-size: 30px;
+export const Body = styled.div`
+  text-align: left;
+  font-family: "Adobe Garamond W08", Times;
+  font-weight: regular;
+  font-size: 22px;
+  line-height: 30px;
+  max-width: 680px;
+  margin: 10vh auto;
 `;
+
+export const Link = styled.span`
+  text-decoration: underline;
+  text-decoration-style: dashed;
+  cursor: pointer;
+`;
+
+
+injectGlobal`
+  @import url('https://webfonts.artsy.net/unica-webfonts.css');
+  @import url('https://webfonts.artsy.net/force-webfonts.css?a=b');
+  }
+`
