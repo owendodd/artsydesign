69300a072a873f019bbce1061dfc730cb9febaf1
Owen <owen.dodd@gmail.com>
2018-04-23 16:52:20 -0400

    tool styles

--- learn/tooltips/v1/src/App.js
+++ learn/tooltips/v1/src/App.js
@@ -1,34 +1,56 @@
 import React, { Component } from "react";
 import ReactTooltip from "react-tooltip";
-import { Body, Link } from "./AppStyles";
+import AppStyles from "./AppStyles";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Body>
+      <AppStyles>
+        <div className="Body">
           Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
           vestibulum. Maecenas sed diam eget risus varius blandit sit amet non
           magna. Donec ullamcorper nulla non metus auctor fringilla. Nullam id
           dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a
           ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget
           urna mollis ornare vel eu leo.{" "}
-          <Link data-tip data-for="nick">
+          <div className="Link" data-tip data-for="nick" >
             Nick Mauss
-          </Link>{" "}
+          </div>{" "}
           commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
           risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
           consectetur ac, vestibulum at eros. Curabitur blandit tempus
           porttitor. Nullam id dolor id nibh ultricies vehicula ut id elit.
           Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna
           mollis ornare vel eu leo.
-        </Body>
+        </div>
 
-        <ReactTooltip className="custom" id="nick" aria-haspopup="true" effect='solid'>
-          <p>Nick Mauss was here</p>
+        <ReactTooltip
+          className="custom"
+          id="nick"
+          type="light"
+          delayShow={400}
+          delayHide={2000}
+          aria-haspopup="true"
+          effect="solid"
+        >
+          <div className="Images">
+            <img src="https://d32dm0rphc51dk.cloudfront.net/eYGNRMFqIirK-962fSOAsw/larger.jpg" />
+            <img src="https://d32dm0rphc51dk.cloudfront.net/_lnI3nNFEo6D9TFqm2oM1w/larger.jpg" />
+          </div>
+          <div className="Info">
+            <div className="Name">Nick Mauss</div>
+            <div className="Follow">+ Follow</div>
+            <div className="Nation">American, b. 1980</div>
+            <div className="Bio">
+              Nick Mauss makes drawings, prints, and paintings that often take
+              on sculptural presence in their presentation; his 2012 large-scale
+              series of silkscreen prints…{" "}
+              <div className="ReadMore">Read More</div>
+            </div>
+          </div>
         </ReactTooltip>
 
-      </div>
+      </AppStyles>
     );
   }
 }
--- learn/tooltips/v1/src/AppStyles.js
+++ learn/tooltips/v1/src/AppStyles.js
@@ -1,21 +1,98 @@
 import styled from 'styled-components';
 import { injectGlobal } from 'styled-components';
 
-export const Body = styled.div`
-  text-align: left;
-  font-family: "Adobe Garamond W08", Times;
-  font-weight: regular;
-  font-size: 22px;
-  line-height: 30px;
-  max-width: 680px;
-  margin: 10vh auto;
-`;
+export default styled.div`
+  .Body {
+    text-align: left;
+    font-family: "Adobe Garamond W08", Times;
+    font-weight: regular;
+    font-size: 22px;
+    line-height: 30px;
+    max-width: 680px;
+    margin: 10vh auto;
+  }
+  .Link {
+    text-decoration: underline;
+    width: auto;
+    display: inline-block;
+    text-decoration-style: dashed;
+    cursor: pointer;
+  }
 
-export const Link = styled.span`
-  text-decoration: underline;
-  width: auto;
-  text-decoration-style: dashed;
-  cursor: pointer;
+  .__react_component_tooltip {
+    background-color: #fff;
+    max-width:340px;
+    opacity: 0;
+    color: #000;
+    /* border: 1px solid #e5e5e5; */
+    border-radius: 0;
+    transition: all 250ms;
+    padding: 20px;
+    box-shadow: 0 0 15px 0 rgba(0,0,0,0.15);
+  }
+    
+    .Images {
+      width: 320px;
+      display: flex;
+      white-space: nowrap;
+      margin-bottom: 10px;
+    }
+      .Images img {
+        margin-right: 10px;
+      }
+      
+      #nick .Images img{
+        height: 161px;
+      }
+
+    .Info {
+      position: relative;
+    }
+      .Name {
+        font-family:"Adobe Garamond W08";
+        font-weight: 600;
+        font-size: 18px;
+        line-height: 20px; 
+        margin-bottom: 3px;
+      }
+      .Nation {
+        font-family: "Unica77LLWebMedium";
+        font-size: 12px;
+        line-height: 14px;
+        margin-bottom: 10px
+      }
+      .Bio {
+        font-family:"Adobe Garamond W08";
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 18px; 
+      }
+      .ReadMore {
+        text-decoration: underline;
+        display: inline-block;
+      }
+      .Follow {
+        font-family: "Unica77LLWebMedium";
+        font-size: 12px;
+        line-height: 14px; 
+        display: block;
+        float: right;
+        right: 0px;
+        cursor: pointer;
+        top: 0px;
+        position: absolute;
+        padding: 5px 10px;
+        border: 1px solid #e5e5e5;
+      }
+      .Follow:hover {
+        color: #fff;
+        background-color: #000;
+      }
+
+  .__react_component_tooltip.show {
+    opacity: 1;
+  }
+
 `;
 
 
