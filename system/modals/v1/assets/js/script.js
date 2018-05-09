81c46182d0d820d350d694825d54deb829038e74
Owen <owen.dodd@gmail.com>
2018-05-09 17:32:56 -0400

    modal progress

--- system/modals/v1/assets/js/script.js
+++ system/modals/v1/assets/js/script.js
@@ -15,9 +15,32 @@ $('.close').click(function() {
 
 
 
-$('.button').click (function() {
+// $('.button').click (function() {
 
-  $('.screen').toggleClass('hidden');
-  $('.screen').toggleClass('off');
+//   $('.screen').toggleClass('hidden');
 
-})
+// })
+
+$('.modal').css({ 'height': $('.modal').outerHeight() });
+
+    $('.button').click(function() {
+      var target          = $(this).data('target'),
+          modal_height    = 180,
+          $current_screen = $('.screen.show');
+          next_target     = find_next_screen(target);
+          
+      $current_screen.removeClass('show');
+      $(target).addClass('show');
+      modal_height = modal_height + $(target).outerHeight();
+      $('.modal').css({ 'height': modal_height });
+      $('.button').data('target', next_target);
+    });
+
+    function find_next_screen(target) {
+      var total_screens    = $('.screen').length,
+          current_position = $(target).index() + 1,
+          next_position    = current_position == total_screens ? 1 : current_position + 1,
+          next_target      = '.screen-' + next_position;
+      
+      return next_target;
+    }
