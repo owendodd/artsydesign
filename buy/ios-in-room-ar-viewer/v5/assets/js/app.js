c4aeff90ec967446bbbaf9e74536c4e2eba285d3
Brian Watterson <briansw@gmail.com>
2018-05-10 17:40:32 -0400

    ar flow

---
+++ buy/ios-in-room-ar-viewer/v5/assets/js/app.js
@@ -0,0 +1,36 @@
+initialize_step_one();
+
+$('.button').click(function() {
+  var target = $(this).data('target'),
+      parent = $(this).parent(),
+      step   = $(this).data('step');
+
+  $(parent).addClass('complete');
+  $('.screen').removeClass('show');
+  $(target).addClass('show');
+  $('h2').html(step);
+  
+  if ($(this).hasClass('done')) {
+    $('.modal').removeClass('show');
+    $('.button.reset').addClass('show');
+  } else if ($(this).hasClass('reset')) {
+    $('.modal').addClass('show');
+    $(this).removeClass('show');
+    $('.screen').removeClass('complete');
+    initialize_step_one();
+  }
+});
+
+setTimeout(function() { 
+  $('.modal').addClass('show');
+}, 1000);
+
+function initialize_step_one() {
+  setTimeout(function() {
+    $('.step-state img').toggleClass('show');
+    setTimeout(function () {
+      $('.step-state img').toggleClass('show');
+      $('.button.hidden').click();
+    }, 700);
+  }, 3000);
+}
